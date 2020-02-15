33415bb38b106b42c88a76928d7ef3d8f423dcb5
stefaleon <stefaleon@gmail.com>
2020-02-15 13:06:49 +0200

    split the data to rows and each row to an array of comma separated values

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import fs from "fs";
 
-const matches = fs.readFileSync("football.csv", { encoding: "utf-8" });
+const matches = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((row: string): string[] => row.split(","));
 
 console.log(matches);
