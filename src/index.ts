87202b6db7ae09b10e4aa26b7796ead7d65329c5
stefaleon <stefaleon@gmail.com>
2020-02-15 23:03:08 +0200

    refactor to a composition approach with the DataReader interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader("football.csv");
-reader.read();
-const matches = reader.data;
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+const matches = matchReader.matches;
 // console.log(matches);
 
 let manUnitedWins = 0;
