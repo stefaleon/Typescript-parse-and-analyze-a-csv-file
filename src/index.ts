2d2527eee0f37ee654a976aa2f4f15706bb92b27
stefaleon <stefaleon@gmail.com>
2020-02-15 16:31:29 +0200

    analyze and report on Manchester United wins

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,27 @@ const matches = fs
   .split("\n")
   .map((row: string): string[] => row.split(","));
 
-console.log(matches);
+// console.log(matches);
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
+
+let manUnitedWins = 0;
+let manUnitedHomeWins = 0;
+let manUnitedAwayWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedHomeWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedAwayWins++;
+  }
+}
+
+manUnitedWins = manUnitedHomeWins + manUnitedAwayWins;
+console.log(
+  `Manchester United won a total of ${manUnitedWins} games, ${manUnitedHomeWins} at home and ${manUnitedAwayWins} away.`
+);
